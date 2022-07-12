86b019f12de9afad132deb9138584693282f10a4
Ollololl Ollooloo <31069733+ollolollollooloo@users.noreply.github.com>
2022-07-12 16:36:01 +0800

    Update nature-lover.js

--- nature-lover.js
+++ nature-lover.js
@@ -21,8 +21,8 @@ function nature_lover(up_speed, down_speed, target_height) {
     
     return day
 }
-console.log(nature_lover(100,10,910));
-// console.log(nature_lover(5, 2, 19));
-// console.log(nature_lover(8, 7, 23));
-// console.log(">>>>>result: ",nature_lover(14, 7, 28));
-// console.log(nature_lover(3, 1, 10));
+console.log(nature_lover(100,10,910)); // result: 10
+// console.log(nature_lover(5, 2, 19)); // result: ?
+// console.log(nature_lover(8, 7, 23)); // result: 10
+// console.log(">>>>>result: ",nature_lover(14, 7, 28)); // result: ?
+// console.log(nature_lover(3, 1, 10)); // result: ?
