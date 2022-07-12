4d3bc38dbe401834e71d63c76277656a75089af4
Ollololl Ollooloo <31069733+ollolollollooloo@users.noreply.github.com>
2022-07-12 16:41:08 +0800

    Update nature-lover.js

--- nature-lover.js
+++ nature-lover.js
@@ -23,6 +23,6 @@ function nature_lover(up_speed, down_speed, target_height) {
 }
 console.log(nature_lover(100,10,910)); // result: 10
 // console.log(nature_lover(5, 2, 19)); // result: ?
-// console.log(nature_lover(8, 7, 23)); // result: 10
+// console.log(nature_lover(8, 7, 23)); // result: ?
 // console.log(">>>>>result: ",nature_lover(14, 7, 28)); // result: ?
 // console.log(nature_lover(3, 1, 10)); // result: ?
