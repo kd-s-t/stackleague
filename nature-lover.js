7244da42b6263f8ee9bbf499ef6881df706f19cc
Ollololl Ollooloo <31069733+ollolollollooloo@users.noreply.github.com>
2022-07-12 14:44:20 +0800

    Create nature-lover.js

---
+++ nature-lover.js
@@ -0,0 +1,28 @@
+function nature_lover(up_speed, down_speed, target_height) {
+    let height_now = 0
+    let day = 0
+    while(height_now <= target_height){
+      console.log("------------------newday------------------");
+      day++
+      height_now = height_now + up_speed
+      height_now = height_now - down_speed
+      console.log("day",day);
+      console.log("height_now",height_now);
+      console.log("target_height",target_height);
+      if(height_now===target_height){
+        console.log("break");
+        break
+      }
+      if(height_now>target_height){
+        console.log("goback");
+        day--
+      }
+    }
+    
+    return day
+}
+console.log(nature_lover(100,10,910));
+// console.log(nature_lover(5, 2, 19));
+// console.log(nature_lover(8, 7, 23));
+// console.log(">>>>>result: ",nature_lover(14, 7, 28));
+// console.log(nature_lover(3, 1, 10));
